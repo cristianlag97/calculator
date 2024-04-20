d89bef3c5d0336265c0c716c913bf1209923b019
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2024-04-20 17:54:31 -0500

    Initial Commit

---
+++ Calculadora1/Calculadora1App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculadora1App.swift
+//  Calculadora1
+//
+//  Created by Cristian David Laguna Aldana on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculadora1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
